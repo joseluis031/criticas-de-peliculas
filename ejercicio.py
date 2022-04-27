74b15005beabcdff666b35645fe9e2a4dda0ebfd
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-27 19:55:59 +0200

    selecciono la columna de donde voy a sacar l media

--- ejercicio.py
+++ ejercicio.py
@@ -7,8 +7,12 @@ class ejercicio:
         self.datos = datos
         
     def calculo_media(self):
-        with open('calificaciones.csv') as self.File:
+        with open('cine.csv') as self.File:
             reader = csv.reader(self.File, delimiter=';')
             next(reader, None)
+            lista = []
             for i in reader:
+                cantidad_votos = i[1]
+                self.lista.append(i)
+                print(f"{cantidad_votos}")
                 
