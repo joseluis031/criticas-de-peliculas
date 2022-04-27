38fb861a68e3d9d5ec24f2dc8377dc3caa6bac7b
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-27 19:37:16 +0200

    leo el csv

--- ejercicio.py
+++ ejercicio.py
@@ -1,10 +1,14 @@
 from collections import Counter
 from math import *
 import matplotlib.pyplot as plt
-
+import csv
 class ejercicio:
     def __init__(self,datos):
         self.datos = datos
         
     def calculo_media(self):
-        
+        with open('calificaciones.csv') as self.File:
+            reader = csv.reader(self.File, delimiter=';')
+            next(reader, None)
+            for i in reader:
+                
