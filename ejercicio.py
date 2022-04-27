ebf8e6a81ed3823941e28854626a2883cbad0905
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-27 22:39:55 +0200

    Tenemos la media

--- ejercicio.py
+++ ejercicio.py
@@ -1,10 +1,14 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-class ejercicio:
+class Ejercicio:
     def __init__(self,datos):
         self.datos = pd.read_csv(datos)
         
     def calculo_media(self):
         suma = self.datos["Productos(Ni*Xi)"].sum()
         N = self.datos["Cantidad de votantes(Ni)"].sum()
+        return suma/N
+
+ejercicio = Ejercicio("Cine1c.csv")
+print(ejercicio.calculo_media())
