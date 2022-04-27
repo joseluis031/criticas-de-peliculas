c9f698a7b4117f8c54a22f929914cf6845f46198
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-27 19:08:25 +0200

    constructor

---
+++ ejercicio.py
@@ -0,0 +1,10 @@
+from collections import Counter
+from math import *
+import matplotlib.pyplot as plt
+
+class ejercicio:
+    def __init__(self,datos):
+        self.datos = datos
+        
+    def calculo_media(self):
+        
